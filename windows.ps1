d4a39360031741776c8e8c1a816692e620fd4d41
Railander Marques <62389758+railandermarques@users.noreply.github.com>
2021-02-26 10:39:58 -0300

    Create windows.ps1

---
+++ windows.ps1
@@ -0,0 +1,5 @@
+Uninstall-WindowsFeature -Name Remote-desktop-services -IncludeManagementTools -restart
+Set-ItemProperty ‘HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\‘ -Name “fDenyTSConnections” -Value 0
+Set-ItemProperty ‘HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\‘ -Name “UserAuthentication” -Value 1
+Enable-NetFirewallRule -DisplayGroup “Remote Desktop”
+shutdown -r
